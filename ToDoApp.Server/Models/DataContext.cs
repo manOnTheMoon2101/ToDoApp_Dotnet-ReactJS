c06a018610872468bb8444a36fd5bee1fdfabf86
manOnTheMoon2101 <clevejohnclayton.2101@gmail.com>
2023-11-23 17:23:40 +0200

    Add project files.

---
+++ ToDoApp.Server/Models/DataContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ToDoApp.Server.Models;
+
+public partial class DataContext : DbContext
+{
+    public DataContext()
+    {
+    }
+
+    public DataContext(DbContextOptions<DataContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Person> People { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=THEFALCON\\MSSQLSERVER01;Initial Catalog=Data;Integrated Security=True;Trust Server Certificate=True;Encrypt=False");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Person>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__People__3213E83F616CF4C8");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Date).HasColumnName("date");
+            entity.Property(e => e.Message)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("message");
+            entity.Property(e => e.Name)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("name");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ todoapp.client/src/App.jsx
@@ -0,0 +1,103 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import axios from "axios";
+
+
+
+function App() {
+
+    const [data, setData] = useState([]);
+
+    const getData = () => {
+        axios.get("api/data/GetData")
+            .then((result) => {
+                setData(result.data)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+
+    }
+
+    useEffect(() => {
+        getData();
+
+    }, [])
+
+    const [inputs, setInputs] = useState({});
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios
+            .post("api/data/GetData", inputs)
+            .then((res) => {
+                console.log(res);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+            .finally(() => {
+                setInputs({});
+
+            });
+    };
+
+    const handleChange = (e) => {
+        const name = e.target.name;
+        const value = String(e.target.value);
+        setInputs((prevState) => ({ ...prevState, [name]: value }));
+    };
+
+
+
+
+
+
+
+    return (
+        <>
+            <h2>Home</h2>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Name
+                    <input name='name' value={inputs.name || ""} onChange={handleChange} />
+
+                </label>
+
+
+                <label>
+                    Message
+                    <input name='message' value={inputs.message || ""} onChange={handleChange} />
+
+                </label>
+
+                <label>
+                    Date
+                    <input name='date' value={inputs.date || ""} onChange={handleChange} />
+
+                </label>
+
+                <button type='submit'>Submit</button>
+
+            </form>
+
+
+            {data.map((x) => (
+                <>
+                    <p>{x.id}</p>
+                    <p>{x.name}</p>
+                    <p>{x.message}</p>
+                    <button>Delete</button>
+                    <button>Edit</button>
+                    <p>{x.date}</p>
+
+
+                </>
+            ))}
+        </>
+    );
+
+
+}
+
+export default App;
